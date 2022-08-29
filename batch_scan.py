d25cea1013da92dfd78c6f6c1b07626d0052d5c4
Francois Laboureur <6578006+flabou@users.noreply.github.com>
2022-08-29 15:44:15 +0200

    Simplified batch scan script to output a pdf without OCR

--- batch_scan.py
+++ batch_scan.py
@@ -7,45 +7,22 @@ scan stops when the feeder is empty, and the DS-640 doesn't have a real feeder
 (only one document is present in the "feeder").
 
 The scanning occurs in the following steps:
-    1. Scan images as long as the user asks for it, in 300dpi resolution in 
-       separate tiff image files.
-    2. For each page, perform OCR on the document using tesseract in hocr format,
-       then convert hocr file to djvu compatible hidden text format.
-    3. If the output config is set to bitonal (currently the only mode 
-       implemented), convert the file to .pbm image format which is expected by
-       cjb2 for djvu conversion.
-    4. Convert files to djvu, and merge them with respective ocr
-    5. Merge all djvu files to a single multipage file.
-    6. Remove temporary files.
-
-To add:
-    metadata (scan date in djvu hidden text)
+    1. Scan images as long as the user asks for it, in 300dpi resolution in jpg files.
+    2. Merge files as a single pdf file.
+    3. Remove temporary files.
 
 dependencies:
     - sane and the scanimage command to perform the scan
-    - tesseract to perform ocr on the pdf file
     - magick to convert image from one format to another
-    - beautifulsoup to parse hocr file and convert it to djvu txt file
 """
 import os
 import sys
+import shlex
+from subprocess import check_output
 
-from hocr2djvu import hocr_to_djvutxt
-
-# import argparse
-
-# parser = argparse.ArgumentParser(description="Batch scan mode for brother ds-640 using scanimage.")
-# parser.add_argument('
-
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
-# Error codes returned by scanimage. This is not very pretty as I am not sure they
-# won't change with updates to sane, but this will do and is an easy fix in case
-# they change.
-SCANNER_NOT_FOUND = 256
-FEEDER_EMPTY = 1792
-FEEDER_JAMMED = 1536
+# Quality of jpg can be reduce to produce smaller size pdf files
+tiff_to_jpg_cmd = "magick {0}.tiff -quality 100 {0}.jpg"
+merge_cmd = "magick {0} {1}"
 
 # Document sizes:
 # | Document          | width [mm] | height [mm] |
@@ -64,81 +41,72 @@ FEEDER_JAMMED = 1536
 
 # Uncomment one of these lines comment the others
 scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 210 -y 297 --resolution=300 -o {}" # A4 - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 210 -y 148 --resolution=300 -o {}" # A5 - Paysage
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 148 -y 210 --resolution=300 -o {}" # A5 - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 148 -y 105 --resolution=300 -o {}" # A6 - Paysage
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 105 -y 148 --resolution=300 -o {}" # A6 - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 105 -y 74  --resolution=300 -o {}" # A7 - Paysage
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 74  -y 105 --resolution=300 -o {}" # A7 - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 74  -y 52  --resolution=300 -o {}" # A8 - Paysage
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 52  -y 74  --resolution=300 -o {}" # A8 - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 86  -y 55  --resolution=300 -o {}" # Carte de credit   - Portrait
-#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=no -x 106 -y 283 --resolution=300 -o {}" # Attestation Soins - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 210 -y 148 --resolution=300 -o {}" # A5 - Paysage
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 148 -y 210 --resolution=300 -o {}" # A5 - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 148 -y 105 --resolution=300 -o {}" # A6 - Paysage
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 105 -y 148 --resolution=300 -o {}" # A6 - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 105 -y 74  --resolution=300 -o {}" # A7 - Paysage
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 74  -y 105 --resolution=300 -o {}" # A7 - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 74  -y 52  --resolution=300 -o {}" # A8 - Paysage
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 52  -y 74  --resolution=300 -o {}" # A8 - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 86  -y 55  --resolution=300 -o {}" # Carte de credit   - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev2' --AutoDeskew=yes --AutoDocumentSize=no -x 106 -y 283 --resolution=300 -o {}" # Attestation Soins - Portrait
+#scan_cmd = "scanimage -d 'brother5:bus2;dev1' --AutoDeskew=yes --AutoDocumentSize=yes --resolution=300 -o {}" # Auto - Portrait
 
-tiff_to_pbm_cmd = "magick {0}.tiff -threshold 70% {0}.pbm"
-merge_ocr_cmd = "djvused -e 'select 1; set-txt {0}.djvutxt; save' {0}.djvu"
-merge_cmd = "djvm -c {}.djvu *.djvu"
+def exec_cmd(cmd, stderr=None):
+    cmd_list = shlex.split(cmd)
+    return check_output(cmd_list, stderr=stderr).decode()
 
-# After a lot of experimentation, I found that the best compression format was 
-# djvu bitonal using cjb2 command. Files are small enough that 300dpi is 
-# acceptable, although 150dpi is still readable. Below, quality is too bad.
-# Reducing dpi must be done before using cjb2, (for instance using 
-# magick -resize 50%) as the setting given to cjb2, is a metadata used for 
-# scaling of the image.
-bitonal_compress_cmd = "cjb2 {0}.pbm {0}.djvu -losslevel 100"
-# bitonal_compress_cmd = "cjb2 {0}.pbm {0}.djvu"
-ocr_cmd = "tesseract -l fra {} {} hocr"
+
+original_dir = os.getcwd()
+working_dir = exec_cmd("mktemp -d").strip()
+os.chdir(working_dir)
 
 ret = 1
 i = 1
 stop = False
 
-msg_string = "q: stop and cancel\ns: stop and save\nnumber: go back to this page\nanything else: scan page {} and keep going."
+msg_string = "q: cancel; s: end and save; number: go back to page; empty string: scan page {} and keep going."
 
-scans = []
-
+last_page = 0
 while stop == False:
     print(msg_string.format(i, i))
     cmd = input().strip()
 
-    if cmd == "q":
+    # Scan next page
+    if cmd == "":
+        try:
+            out = "{}.tiff".format(i)
+            exec_cmd(scan_cmd.format(out))
+            if(i > last_page):
+                last_page = i
+
+            ret = exec_cmd(tiff_to_jpg_cmd.format(i))
+            i+=1 
+        
+        except:
+            print("Scan of page {} failed".format(i))
+
+    # Exit without saving
+    elif cmd == "q":
         stop = True
         print("Exited without saving output")
+        print(exec_cmd(("rm -r {}".format(working_dir))))
+
+    # Save result i.e. merge
     elif cmd == "s":
         stop = True
 
-        # Go through all scans, convert hocr to djvutxt if it exists, then write
-        # the result to djvu page.
-        for (p, filename) in scans:
-            os.system(bitonal_compress_cmd.format(p))
-            try:
-                with open("{}.hocr".format(p)) as fp:
-                    hocr_txt = fp.read()
-                    djvu_txt = hocr_to_djvutxt(hocr_txt)
-                    
-                with open("{}.djvutxt".format(p), "w") as fp:
-                    fp.write(djvu_txt)
+        page_sequence = " ".join([str(k) + ".jpg" for k in range(1, last_page+1)])
+        print(exec_cmd(merge_cmd.format(page_sequence, "output.pdf")))
+        print(exec_cmd(("mv {} {}".format("output.pdf", original_dir))))
+        os.chdir(original_dir)
+        print(exec_cmd(("rm -r {}".format(working_dir))))
+        print("File saved to output.pdf")
 
-                print("Adding ocr data to page {}".format(p))
-                os.system(merge_ocr_cmd.format(p))
-
-
-            except:
-                # TODO: Check that the process has returned somehow?
-                eprint("Unable to open {}.hocr, maybe tesseract didn't finish".format(i))
-        os.system(merge_cmd.format("0"))
-        print("File saved to 0.djvu")
-
-        # ret = os.system(merge_cmd)  
     elif cmd.isdigit() and (n := int(cmd)) > 0:
         i = n
 
+    # Any other command is ignored
     else:
-        out = "{}.tiff".format(i)
-        ret = os.system(scan_cmd.format(out))
-        if ret == 0:
-            scans.append((i, out))
-            ret = os.system(ocr_cmd.format(out, i))
-            ret = os.system(tiff_to_pbm_cmd.format(i))
-            i+=1
-
+        print("Unknown command '{}'".format(cmd))
